09760eda69a08d4f18b1f30dc5a73bad620d63e4
Marsh542 <nikstudyuni@gmail.com>
2025-09-17 23:43:56 +1000

    Added base variables and methods: a start to the logic implementaion of testpage for Antarctica

--- src/main/java/com/example/geoquestkidsexplorer/controllers/TestModeAntarcticaController.java
+++ src/main/java/com/example/geoquestkidsexplorer/controllers/TestModeAntarcticaController.java
@@ -8,15 +8,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TestModeAntarcticaController {
 
+//FXML variables, linked to FXML file
+    @FXML public Label timerLabel;
+    @FXML public Label scoreLabel;
+    @FXML public ImageView countryImageView;
+    @FXML public Label questionLabel;
+    @FXML public VBox quizOptionsContainer;
+    @FXML public TextField answerField;
+    @FXML public Button nextQuestionButton;
 
-    public Label timerLabel;
-    public Label scoreLabel;
+
+    public void backToGameModes(ActionEvent actionEvent) {
+
+    }
+
+    public void nextQuestion(ActionEvent actionEvent) {
+
+    }
 }
